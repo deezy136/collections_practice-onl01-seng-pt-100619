e0944770932b3eec6dce09d60c9532990454b3ff
deezy13 <deezy13@gmail.com>
2019-10-17 03:33:58 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,24 @@
+def sort_array_asc(array)
+  
+sorted_asc = array.sort do | a, b |
+       a <=> b 
+  end 
+    sorted_asc
+end 
+
+def sort_array_desc(array)
+  
+sorted_desc = array.sort do | a, b |
+       b <=> a
+  end 
+    sorted_desc
+end 
+
+def sort_array_char_count(array)
+  
+ sorted_char = array.sort do | a, b |
+       a.size <=> b.size
+  end 
+    sorted_char
+end 
 
